a32abd2943fac5fe2851199a4705f7f5cbc8b969
Bahar <baharsoufilo@gmail.com>
2024-01-28 23:25:44 +0330

    small fix

--- MovieReviewSite.Core/Services/Movie/MovieBaseService.cs
+++ MovieReviewSite.Core/Services/Movie/MovieBaseService.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MovieReviewSite.Core.Interfaces.Movie;
-using MovieReviewSite.Core.Models;
-using MovieReviewSite.Core.Models.Movie;
+﻿using MovieReviewSite.Core.Interfaces.Movie;
 using MovieReviewSite.DataBase.Contexts;
 
 namespace MovieReviewSite.Core.Services.Movie;
@@ -9,11 +6,9 @@ namespace MovieReviewSite.Core.Services.Movie;
 public partial class MovieBaseService :IMovieBaseService
 {
     private readonly ReviewSiteContext _context;
-    private IMovieBaseService _movieBaseServiceImplementation;
 
     public MovieBaseService(ReviewSiteContext context1, IMovieBaseService movieBaseServiceImplementation)
     {
         _context = context1;
-        _movieBaseServiceImplementation = movieBaseServiceImplementation;
     }
 }
